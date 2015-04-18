3a1b4ec18b759d3bafda07065b2106e6077e5366
morganhowell95 <morgan.howell95@gmail.com>
2015-04-18 03:21:56 -0400

    general program for botlibre

--- LonelyNoMore.py
+++ LonelyNoMore.py
@@ -2,14 +2,12 @@ import requests.packages.urllib3.contrib.pyopenssl
 requests.packages.urllib3.contrib.pyopenssl.inject_into_urllib3()
 import xml.etree.ElementTree as ET
 
-user_input = raw_input("Hi, you've summoned Albert Einstein!\n")
-while(user_input!='go away' and user_input!='stop'):
         #The following lines create the structure of our GET request which is made up of our specific keys, headers, and attributes
-        instance='160'
+        instance='INSERT THE ID OF THE BOT YOU WANT TO CREATE'
         message=user_input
         message = message.replace(' ', '+')
-        user='mhowell95'
-        password='606744HMj'
+        user='INSERT YOUR USERNAME'
+        password='INSERT YOUR PASSWORD'
         applicationID='3087601344337415072'
         #formatting our information into a URL
         url = "http://www.botlibre.com/rest/botlibre/form-chat?instance=%s&message=%s$user=%s&password=%s&application=%s" % \
@@ -21,10 +19,4 @@ while(user_input!='go away' and user_input!='stop'):
         root = ET.fromstring(response.text)
         response_message = root[0].text
 
-        #printing the clean, parsed result
-        print response_message
-          
-        #retrieve user input and repeat loop of the input passes condition
-        user_input = raw_input() 
 
-print 'Cheerio, now!'
